56f10e41fd7c8cf03077f92e7a315094f53134da
shubhampawar574 <64591332+shubhampawar574@users.noreply.github.com>
2023-01-19 22:58:00 +0530

    Create 19_Carpet into Box.cpp

---
+++ Jan/GFG/19_Carpet into Box.cpp
@@ -0,0 +1,13 @@
+int carpetBox(int A, int B, int C, int D){
+        //code here
+        if((A<=C&&B<=D) || (B<=C&&A<=D)){
+            return 0;  
+        } 
+        if(A>C && A>D){
+            return 1+carpetBox(A/2,B,C,D);  
+        } 
+        if(B>C && B>D){
+            return 1+carpetBox(A,B/2,C,D);
+        } 
+        return min(1+carpetBox(A/2,B,C,D), 1+carpetBox(A,B/2,C,D));
+    }
