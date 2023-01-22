6ca435c3c229615837d1832dde6a3631aa469198
shubhampawar574 <64591332+shubhampawar574@users.noreply.github.com>
2023-01-23 00:13:32 +0530

    Create 23_Geeks And The String.cpp

---
+++ Jan/GFG/23_Geeks And The String.cpp
@@ -0,0 +1,19 @@
+string removePair(string s) {
+        // code here
+        stack<char> st;
+        st.push(s[0]);
+        
+        for(int i=1; i<s.size(); i++){
+            if(!st.empty() && st.top()==s[i]) st.pop();
+            else st.push(s[i]);
+        }
+        
+        string ans;
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        if(ans=="") return "-1";
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
