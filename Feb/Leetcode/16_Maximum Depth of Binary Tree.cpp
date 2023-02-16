5aead388093785abf8a60f7e4ab8f4943a80a5dc
shubhampawar574 <64591332+shubhampawar574@users.noreply.github.com>
2023-02-16 23:50:19 +0530

    Create 16_Maximum Depth of Binary Tree.cpp

---
+++ Feb/Leetcode/16_Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,6 @@
+int maxDepth(TreeNode* root) {
+        if(!root)return  0;
+        int left=maxDepth(root->left);
+        int right=maxDepth(root->right);
+       return max(left,right)+1;
+    }
