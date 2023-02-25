43f4c083b70401e26fb30f89eba67600857549ff
shubhampawar574 <64591332+shubhampawar574@users.noreply.github.com>
2023-02-25 23:40:04 +0530

    Create 25_Valid Compressed String.cpp

---
+++ Feb/GFG/25_Valid Compressed String.cpp
@@ -0,0 +1,21 @@
+int checkCompressed(string S, string T) {
+        // code here
+        int num=0,j=0;
+        for(int i=0;i<T.length();i++){
+            if(isdigit(T[i])){
+                num = num*10+(T[i]-'0');
+            }
+            else{
+                if(num){
+                    j+=num;
+                }
+                num=0;
+                if(j>=S.length() or S[j]!=T[i])return 0;
+                j++;
+            }
+        }
+        //below 2 line is for case where T string has last char as number
+        j+=num;
+        if(j!=S.length())return 0;
+        return 1;
+    }
