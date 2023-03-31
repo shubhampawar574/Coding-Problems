3621ea511814aa8f7f4f55764fe45b80c63d450b
ShubhamPawar <64591332+shubhampawar574@users.noreply.github.com>
2023-03-31 19:47:09 +0530

    Create 31_Even Swap.cpp

---
+++ March/GFG/31_Even Swap.cpp
@@ -0,0 +1,14 @@
+vector <int> lexicographicallyLargest(vector <int> &a,int n)
+{
+    // Code here
+    int i=0;
+    while(i<n){
+        int j=i+1;
+        while(j<n && ((a[j]+a[j-1])%2)==0){
+            j++;
+        }
+        sort(a.begin()+i,a.begin()+j,greater<int>());
+        i=j;
+    }
+    return a;
+}
