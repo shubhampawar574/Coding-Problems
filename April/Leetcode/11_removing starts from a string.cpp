0a7062227b43535d84d3576c61116d09bae1eb10
ShubhamPawar <64591332+shubhampawar574@users.noreply.github.com>
2023-04-11 23:04:27 +0530

    Create 11_removing starts from a string.cpp

---
+++ April/Leetcode/11_removing starts from a string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeStars(string s) {
+        int i=0, j=0, n=s.size();
+        while(j<n){
+            if(s[j]=='*') i--;
+            else s[i++]=s[j];
+            j++;        
+        }
+        return s.substr(0, i);
+
+    }
+};
