87fe889af6b094ef737e5261fffa787d41fab766
ShubhamPawar <64591332+shubhampawar574@users.noreply.github.com>
2023-05-21 17:50:37 +0530

    Create 21_Bus Conductor.cpp

---
+++ May/GFG/21_Bus Conductor.cpp
@@ -0,0 +1,9 @@
+int findMoves(int n, vector<int> chairs, vector<int> passengers) {
+    sort(chairs.begin(),chairs.end());
+    sort(passengers.begin(),passengers.end());
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans+=abs(chairs[i]-passengers[i]);
+    }
+    return ans;
+}
