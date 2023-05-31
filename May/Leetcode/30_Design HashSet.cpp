f9c48a5916a60de1ee04c54a38566d43e1f389d5
ShubhamPawar <64591332+shubhampawar574@users.noreply.github.com>
2023-05-31 05:47:34 +0530

    Create 30_Design HashSet.cpp

---
+++ May/Leetcode/30_Design HashSet.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+private:
+    std::vector<bool> mp;
+
+public:
+    MyHashSet() {
+        mp.resize(1000001, false);
+    }
+
+    void add(int key) {
+        mp[key] = true;
+    }
+
+    void remove(int key) {
+        mp[key] = false;
+    }
+
+    bool contains(int key) {
+        return mp[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
